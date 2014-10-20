f4676fbca5f4e6d1a77ed00ecf70116f8c2c12c2
Randall Leung <leungran@gmail.com>
2014-10-20 13:24:32 -0700

    add check if user first time login

--- GithubToGo/SplitContainerViewController.swift
+++ GithubToGo/SplitContainerViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class SplitContainerViewController: UIViewController, UISplitViewControllerDelegate {
+    
+    var userDefaults: NSUserDefaults?
+    var firstTimeLogginIn: Bool?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let splitVC = self.childViewControllers[0] as UISplitViewController
         splitVC.delegate = self
+        
+        self.userDefaults = NSUserDefaults.standardUserDefaults()
+        self.firstTimeLogginIn = self.userDefaults!.objectForKey("firstTimeLogin") as? Bool
+    }
+
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool {
+        
+        if (self.firstTimeLogginIn == nil) {
+            self.userDefaults?.setBool(true, forKey: "firstTimeLogin")
+            return true
+        } else {
+            return false
+        }
+
     }
     
-    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool {
-        return false
-    }
+    
     
 
 
